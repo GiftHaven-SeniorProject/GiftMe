8174c71e8f626bbddc35c1283692e99612be1568
MahiP32 <146895784+MahiP32@users.noreply.github.com>
2024-03-05 18:36:08 -0500

    User Interface changes

---
+++ usergifts_interface/usergifts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var myGiftsAddedBtn = document.getElementById("MyGiftsAdded");
+  myGiftsAddedBtn.addEventListener("click", function() {
+    // Redirect to the specified link
+    window.location.href = "http://staging2.christophero75.sg-host.com/my-account/?nmgr-notice=login-to-access&nmgr-redirect=/my-gift-lists/home/&nmgr-type=gift-registry";
+  });
+});
